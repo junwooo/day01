a8e2d5b3a67e8ec27df6284f33c1a8a86fe8bafc
junwoo1 <junu8936@daum.net>
2019-05-21 11:11:59 +0900

    day01 code basic

---
+++ test.js
@@ -0,0 +1,30 @@
+/*
+var car = {
+    name : "sonata",
+    ph : "500ph",
+    start : function(){
+        console.log("engine is starting");
+    },
+    stop : function(){
+        console.log("engine is stoped");
+    }
+}
+
+console.log(car.start);
+*/
+
+/*
+var car1="s";
+var car2="d";
+var car3="e";
+var cars=["s","d","e"];
+console.log(cars);
+console.log(cars[0]);
+*/
+
+var cars =["b","c"];
+var text = ",";
+var i;
+for (i=0; i<cars.length; i++){
+console.log(cars[i]);
+}
